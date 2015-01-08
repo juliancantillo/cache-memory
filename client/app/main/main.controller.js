156355fc1db4a8f1464884644eac644ceb636b15
Julian Cantillo <umbrasoft@gmail.com>
2015-01-08 03:32:03 -0500

    on going

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,110 @@
 
 angular.module('cachemApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $scope.checkHex = function (n){return/^[0-9A-Fa-f]{1,64}$/.test(n)}
+    $scope.checkBin = function (n){return/^[01]{1,64}$/.test(n)}
+    $scope.Hex2Bin = function (n){if(!$scope.checkHex(n))return 0;return parseInt(n,16).toString(2);}
+    $scope.Bin2Hex = function (n){if(!$scope.checkBin(n))return 0;return parseInt(n,2).toString(16)}
+
+    $scope.word = '7EABC495FE';
+    $scope.binary_word = $scope.Hex2Bin($scope.word);
+
+    $scope.data_units = [
+      { name: "b", factor: 0 },
+      { name: "Kb", factor: 10 },
+      { name: "Mb", factor: 20 },
+      { name: "Gb", factor: 30 },
+    ];
+    $scope.currents = {
+      mm_unit: $scope.data_units[0],
+      cm_unit: $scope.data_units[0],
+      mb_unit: $scope.data_units[0],
+    };
+
+    $scope.values = {
+      mm: { size: 512, unit: $scope.data_units[3] },
+      cm: { size: 64, unit: $scope.data_units[2] },
+      mb: { size: 32, unit: $scope.data_units[0] },
+    };
+    $scope.results = {
+      mm: { log: 13 },
+      cm: { log: 13 },
+      mb: { log: 13 },
+      word: { log: 0 },
+      line: { log: 0 },
+      direct: {},
+    };
+
+    $scope.setMmUnit = function (unit) {
+      $scope.values.mm.unit = unit;
+    }
+    $scope.setCmUnit = function (unit) {
+      $scope.values.cm.unit = unit;
+    }
+    $scope.setMbUnit = function (unit) {
+      $scope.values.mb.unit = unit;
+    }
+
+    $scope.$watchCollection('values.mm', function (new_val) {
+      $scope.results['mm'] = {
+          log: $scope.getLog( new_val.size, new_val.unit.factor )
+        };
+      $scope.results['M'] = $scope.getLog( new_val.size, new_val.unit.factor ) - $scope.results['word'];
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+    $scope.$watchCollection('values.cm', function (new_val) {
+      $scope.results['cm'] = {
+          log: $scope.getLog( new_val.size, new_val.unit.factor )
+        };
+      $scope.results['line'] = $scope.getLog( new_val.size, new_val.unit.factor ) - $scope.results['word'];
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+      $scope.results.direct['line'] = $scope.getLog( new_val.size, new_val.unit.factor ) - $scope.results['word'];
+      $scope.results.direct['tag'] = $scope.results.mm.log - $scope.getLog( new_val.size, new_val.unit.factor );
+
+    });
+
+    $scope.$watchCollection('values.mb', function (new_val) {
+      $scope.results['mb'] = {
+          log: $scope.getLog( new_val.size, new_val.unit.factor )
+        };
+      $scope.results['word'] = $scope.getLog( new_val.size, new_val.unit.factor );
+    });
+
+    $scope.submitForm = function (){
+      var bnw = $scope.binary_word;
+
+      $scope.direct_tag = bnw.substring( 0, $scope.results.direct.tag );
+      $scope.direct_line = bnw.substring( $scope.results.direct.tag, $scope.results.direct.tag + $scope.results.line );
+      $scope.direct_word = bnw.substring( $scope.results.direct.tag + $scope.results.line );
+      $scope.direct_tag_hex = $scope.Bin2Hex( $scope.direct_tag );
+      $scope.direct_line_hex = $scope.Bin2Hex( $scope.direct_line );
+      $scope.direct_word_hex = $scope.Bin2Hex( $scope.direct_word );
+
+      //Asociativa 2 Vias
+      $scope.associative2_tag = bnw.substring( 0, $scope.results.direct.tag + 1 );
+      $scope.associative2_line = bnw.substring( $scope.results.direct.tag + 1, ( $scope.results.direct.tag + 1) + ($scope.results.line - 1 ) );
+      $scope.associative2_word = bnw.substring( ( $scope.results.direct.tag + 1) + ($scope.results.line - 1 ) );
+      $scope.associative2_tag_hex = $scope.Bin2Hex( $scope.associative2_tag );
+      $scope.associative2_line_hex = $scope.Bin2Hex( $scope.associative2_line );
+      $scope.associative2_word_hex = $scope.Bin2Hex( $scope.associative2_word );
+
+      //Asociativa 4 Vias
+      $scope.associative4_tag = bnw.substring( 0, $scope.results.direct.tag + 2 );
+      $scope.associative4_line = bnw.substring( $scope.results.direct.tag + 2, ( $scope.results.direct.tag + 2) + ($scope.results.line - 2 ) );
+      $scope.associative4_word = bnw.substring( ( $scope.results.direct.tag + 2) + ($scope.results.line - 2 ) );
+      $scope.associative4_tag_hex = $scope.Bin2Hex( $scope.associative4_tag );
+      $scope.associative4_line_hex = $scope.Bin2Hex( $scope.associative4_line );
+      $scope.associative4_word_hex = $scope.Bin2Hex( $scope.associative4_word );
+
+      $scope.associative_tag = bnw.substring( 0, $scope.results.M );
+      $scope.associative_word = bnw.substring( $scope.results.M, $scope.results.M + $scope.results.word );
+      $scope.associative_tag_hex = $scope.Bin2Hex( $scope.associative_tag );
+      $scope.associative_word_hex = $scope.Bin2Hex( $scope.associative_word );
+    }
+
+    $scope.getLog = function (value, factor) {
+      return ( Math.log( value ) / Math.log(2) ) + factor;
+    }
+
   });
